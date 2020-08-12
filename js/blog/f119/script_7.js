236e4bea0c3d70deba84375574798fccdf5b2471
Lê Anh Đức <54300988+leanhducprovn@users.noreply.github.com>
2020-08-12 10:41:50 +0700

    Create script_7.js

---
+++ js/blog/f119/script_7.js
@@ -0,0 +1,3 @@
+//<![CDATA[
+if($(".music-box").length) {var musicBtn = $(".music-box-toggle-btn"),musicBox = $(".music-holder");musicBtn.on("click", function() {musicBox.toggleClass("toggle-music-box");return false;})}
+//]]>
