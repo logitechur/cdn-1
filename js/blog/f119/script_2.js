884c234efbbda21ec82cbc45bcc66367bd39e025
Lê Anh Đức <54300988+leanhducprovn@users.noreply.github.com>
2021-06-08 21:59:50 +0700

    Delete js directory

--- js/blog/f119/script_2.js
+++
@@ -1,3 +0,0 @@
-/* <![CDATA[ */
-var flatsomeVars = {"ajaxurl":"","rtl":"","sticky_height":"70","user":{"can_edit_pages":false}};
-/* ]]> */
--- js/blog/f119/script_7.js
+++
@@ -1,3 +0,0 @@
-//<![CDATA[
-if($(".music-box").length) {var musicBtn = $(".music-box-toggle-btn"),musicBox = $(".music-holder");musicBtn.on("click", function() {musicBox.toggleClass("toggle-music-box");return false;})}
-//]]>
