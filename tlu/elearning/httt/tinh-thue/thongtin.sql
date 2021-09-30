fef4c94c70afa1917c8df33f93de2c9df74fe30d
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2021-09-30 15:58:29 +0700

    update sql

---
+++ tlu/elearning/httt/tinh-thue/thongtin.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `thongtin` (
+  `YEAR` varchar(500) COLLATE utf8_vietnamese_ci NOT NULL,
+  `NAME` varchar(500) COLLATE utf8_vietnamese_ci NOT NULL,
+  `BHXH` varchar(500) COLLATE utf8_vietnamese_ci NOT NULL,
+  `MTLD` varchar(500) COLLATE utf8_vietnamese_ci NOT NULL,
+  `MTPT` varchar(500) COLLATE utf8_vietnamese_ci NOT NULL,
+  `SNPT` varchar(500) COLLATE utf8_vietnamese_ci NOT NULL,
+  `TNCN` varchar(500) COLLATE utf8_vietnamese_ci NOT NULL,
+  `TTN` varchar(500) COLLATE utf8_vietnamese_ci NOT NULL,
+  `TTDN` varchar(500) COLLATE utf8_vietnamese_ci NOT NULL,
+  `TTTT` varchar(500) COLLATE utf8_vietnamese_ci NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_vietnamese_ci;
