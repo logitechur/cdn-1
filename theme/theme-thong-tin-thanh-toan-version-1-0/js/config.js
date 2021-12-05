a295652838e534635c1a16bd06ac7454b26703e8
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2021-12-05 13:37:58 +0700

    fix config.js

--- theme/theme-thong-tin-thanh-toan-version-1-0/js/config.js
+++ theme/theme-thong-tin-thanh-toan-version-1-0/js/config.js
@@ -1,50 +1,50 @@
 const USER = {
     banks: [{
             name: "MB BANK",
-            logo: "../images/mbbank.png",
+            logo: "./images/mbbank.png",
             number: "333369693333",
             owner: "LE ANH DUC"
         },
         {
             name: "BIDV BANK",
-            logo: "../images/bidv.png",
+            logo: "./images/bidv.png",
             number: "42710000636335",
             owner: "LE ANH DUC"
         },
         {
             name: "AGRI BANK",
-            logo: "../images/agribank.png",
+            logo: "./images/agribank.png",
             number: "xxxxxxxxxxxx",
             owner: "LE ANH DUC"
         },
         {
             name: "PAYPAL",
-            logo: "../images/paypal.png",
+            logo: "./images/paypal.png",
             number: "leanhducprovn",
             owner: "LE ANH DUC"
         },
         {
             name: "MOMO",
-            logo: "../images/momo.jpeg",
+            logo: "./images/momo.jpeg",
             number: "0386897757",
             owner: "LE ANH DUC",
             fastTransfer: true
         },
         {
             name: "ZALO PAY",
-            logo: "../images/zalopay.png",
+            logo: "./images/zalopay.png",
             number: "0386897757",
             owner: "LE ANH DUC"
         },
         {
             name: "VIETTEL PAY",
-            logo: "../images/viettelpay.png",
+            logo: "./images/viettelpay.png",
             number: "0386897757",
             owner: "LE ANH DUC"
         },
         {
             name: "SHOPEE PAY",
-            logo: "../images/shopeepay.png",
+            logo: "./images/shopeepay.png",
             number: "xxxxxxxxxxxx",
             owner: "LE ANH DUC"
         },
