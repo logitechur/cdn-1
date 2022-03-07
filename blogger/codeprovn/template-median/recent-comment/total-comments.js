924b91d82fe4df898f17992cf5ddb49192012e53
logitechur <101080243+logitechur@users.noreply.github.com>
2022-03-07 19:12:37 +0700

    Delete blogger directory

--- blogger/codeprovn/template-median/recent-comment/total-comments.js
+++
@@ -1,4 +0,0 @@
-function totalComments(json) {
-  document.getElementById('total-comments').innerHTML = json.feed.openSearch$totalResults.$t
-};
-document.write('<script type=\"text/javascript\" src=\"/feeds/comments/default?alt=json-in-script&max-results=0&callback=totalComments\"><\/script>');
